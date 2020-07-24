c509f1e8dc1364b30540489650df08de592d6ff8
peiwal <peiwal@yandex.ru>
2020-07-24 18:43:44 +0400

    adding rest mehtod

--- src/main/java/petrov/delivery/webapi/api/IMobileClientApi.kt
+++ src/main/java/petrov/delivery/webapi/api/IMobileClientApi.kt
@@ -1,8 +1,12 @@
 package petrov.delivery.webapi.api
 
+import petrov.delivery.webapi.ParamRespCompanyInfo
 import petrov.delivery.webapi.ParamRespProduct
 
 interface IMobileClientApi {
     // Запрос списка продуктов
     fun getProducts(): ParamRespProduct
+    
+    // Запрос данных о компании
+    fun getCompanyInfo(): ParamRespCompanyInfo
 }
